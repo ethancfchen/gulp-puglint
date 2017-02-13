be672410f77db9f4c84c7ac022478d078e05a644
ethancfchen <ethan.cfchen@gmail.com>
2017-02-13 17:09:03 +0800

    Add APIs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,5 @@ module.exports = {
     node: true,
     mocha: true,
     es6: true
-  },
-  rules: {
-    'require-jsdoc': 0
   }
 };
---
+++ index.js
@@ -0,0 +1,29 @@
+function gulpPuglint(options) {
+  // TODO
+}
+
+gulpPuglint.result = (action) => {
+  // TODO
+};
+
+gulpPuglint.results = (action) => {
+  // TODO
+};
+
+gulpPuglint.failOnError = () => {
+  // TODO
+};
+
+gulpPuglint.failAfterError = () => {
+  // TODO
+};
+
+gulpPuglint.formatEach = (formatter, writable) => {
+  // TODO
+};
+
+gulpPuglint.format = (formatter, writable) => {
+  // TODO
+};
+
+module.exports = gulpPuglint;
